08c573c04841d8492e19cffb701fa5579e11d943
OMER ABDULLAH <omerabdullah555k@gmail.com>
2024-01-24 21:36:52 +0530

    update code

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World I Omer Abdullah From Swizerland!\n');
+  res.end('Hello, World I Omer Abdullah From Swizerland I am Coming!\n');
 });
 
-const port = 9000;
+const port = 6000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
