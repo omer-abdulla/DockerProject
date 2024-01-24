37b664de82a285c6ae74b9a2830632039812f7ea
OMER ABDULLAH <omerabdullah555k@gmail.com>
2024-01-24 21:43:38 +0530

    update code

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN npm install
 COPY . .
 
 # Expose the port your app will run on
-EXPOSE 9000
+EXPOSE 6000
 
 # Define the command to run your application
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World I Omer Abdullah From Swizerland I am Coming!\n');
+  res.end('Hello, World I Omer Abdullah From Swizerland I am Coming On the way!\n');
 });
 
 const port = 6000;
